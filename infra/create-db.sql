c94018424468cb9b013d7ca56c5559b5fd98199b
Brendon Hablutzel <booklvrbren@gmail.com>
2025-04-06 16:35:28 -0400

    move from regular postgres to timescaledb

--- infra/create-db.sql
+++ infra/create-db.sql
@@ -1,13 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
 CREATE DATABASE argus;
 
 \c argus;
 
 CREATE TABLE IF NOT EXISTS activetabs (
-  id SERIAL PRIMARY KEY,
-  "timestamp" TIMESTAMPTZ,
-  title TEXT,
-  "url" TEXT,
-  "status" VARCHAR(20),
+  "timestamp" TIMESTAMPTZ NOT NULL,
+  title TEXT NOT NULL,
+  "url" TEXT NOT NULL,
+  "status" VARCHAR(20) NOT NULL,
   -- whether a tab was selected at all. if false, title, url, and status will be empty
-  tab_selected BOOLEAN
+  tab_selected BOOLEAN NOT NULL
 );
+
+SELECT create_hypertable('activetabs', by_range('timestamp'));
