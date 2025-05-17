80a9cdb86f32f86393192e01bbe8d591d2f24c6e
Brendon Hablutzel <booklvrbren@gmail.com>
2025-05-17 00:07:23 -0400

    create helm chart

---
+++ deploy/helm/resources/create-db.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+CREATE DATABASE argus;
+
+\c argus;
+
+CREATE TABLE IF NOT EXISTS activetabs (
+  "timestamp" TIMESTAMPTZ NOT NULL,
+  "url" TEXT NOT NULL,
+  title TEXT NOT NULL,
+  "status" VARCHAR(20) NOT NULL,
+  profile_id TEXT NOT NULL
+);
+
+SELECT create_hypertable('activetabs', by_range('timestamp'));
